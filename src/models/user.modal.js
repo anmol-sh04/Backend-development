1d87937474b1a22ee3e236b7fb5c653513c93091
anmol-sh04 <anmolsharma10204@gmail.com>
2026-07-20 17:03:44 +0530

    add user management features: change password, get current user, update account details, and avatar/cover image handling

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -48,10 +48,10 @@ const userSchema = new Schema(
     { timestamps: true}
 )
 
-userSchema.pre("save", async function(){
-    if(!this.isModified("password")) return
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password, 10)
-    
+    next()
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
